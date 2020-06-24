698b5a18cb2a8e71e405460da6ea059492df3d71
29864892 <48174208+29864892@users.noreply.github.com>
2020-06-24 17:41:40 -0400

    started combining levels

---
+++ Final/assets/assets4/level4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="level4" tilewidth="50" tileheight="50" tilecount="240" columns="20">
+ <image source="lvl4map.png" width="1000" height="600"/>
+</tileset>
